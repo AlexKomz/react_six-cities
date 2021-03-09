e8543c99f12b6030b19cbe5dc673af7dd719786c
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-03-09 16:26:39 +0300

    Добавил моковую структуру данных для предложений

---
+++ src/mocks/offers.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    isPremium: true,
+    inBookmark: false,
+    image: {src: `img/apartment-01.jpg`},
+    price: {
+      value: 120,
+      text: `night`
+    },
+    rating: 4,
+    name: `Beautiful &amp; luxurious apartment at great location`,
+    type: `Apartment`
+  }, {
+    isPremium: false,
+    inBookmark: true,
+    image: {src: `img/room.jpg`},
+    price: {
+      value: 80,
+      text: `night`
+    },
+    rating: 4,
+    name: `Wood and stone place`,
+    type: `Private room`
+  }, {
+    isPremium: false,
+    inBookmark: false,
+    image: {src: `img/apartment-02.jpg`},
+    price: {
+      value: 132,
+      text: `night`
+    },
+    rating: 4,
+    name: `Canal View Prinsengracht`,
+    type: `Apartment`
+  }, {
+    isPremium: true,
+    inBookmark: false,
+    image: {src: `img/apartment-03.jpg`},
+    price: {
+      value: 180,
+      text: `night`
+    },
+    rating: 5,
+    name: `Nice, cozy, warm big bed apartment`,
+    type: `Apartment`
+  },
+];
