532e6b1a8f60902cf4903511f5654ae9eb4bde7c
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-03-10 11:22:14 +0300

    Добавил моковые данные и отправил в компонент

--- src/index.js
+++ src/index.js
@@ -2,27 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/app/app.jsx";
+import offers from "./mocks/offers.js";
 
-
-const PLACES = [
-  {
-    name: `Beautiful &amp; luxurious apartment at great location`,
-  },
-  {
-    name: `Wood and stone place`,
-  },
-  {
-    name: `Canal View Prinsengracht`,
-  },
-  {
-    name: `Nice, cozy, warm big bed apartment`,
-  },
-];
 
 ReactDOM.render(
     <App
-      places={PLACES}
-      onPlacesNameClick={() => {}}
+      offers={offers}
     />,
     document.querySelector(`#root`)
 );
