43e4d31c0ac2b3ee3593d0120f6bae38f47454a9
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-03-10 11:21:48 +0300

    Изменил пропсы в компоненте App и заменил разметку на компонент

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import OffersList from "../offers-list/offers-list.jsx";
+
 
 const App = (props) => {
-  const {places, onPlacesNameClick} = props;
+  const {offers} = props;
 
   return (
     <div>
@@ -88,46 +90,9 @@ const App = (props) => {
                     <li className="places__option" tabIndex={0}>Top rated first</li>
                   </ul>
                 </form>
-                <div className="cities__places-list places__list tabs__content">
-                  {places.map((place, index) => {
-                    return (
-                      <article key={place.name + index} className="cities__place-card place-card">
-                        <div className="place-card__mark">
-                          <span>Premium</span>
-                        </div>
-                        <div className="cities__image-wrapper place-card__image-wrapper">
-                          <a href="#">
-                            <img className="place-card__image" src="img/apartment-01.jpg" width={260} height={200} alt="Place image" />
-                          </a>
-                        </div>
-                        <div className="place-card__info">
-                          <div className="place-card__price-wrapper">
-                            <div className="place-card__price">
-                              <b className="place-card__price-value">€120</b>
-                              <span className="place-card__price-text">/&nbsp;night</span>
-                            </div>
-                            <button className="place-card__bookmark-button button" type="button">
-                              <svg className="place-card__bookmark-icon" width={18} height={19}>
-                                <use xlinkHref="#icon-bookmark" />
-                              </svg>
-                              <span className="visually-hidden">To bookmarks</span>
-                            </button>
-                          </div>
-                          <div className="place-card__rating rating">
-                            <div className="place-card__stars rating__stars">
-                              <span style={{width: `80%`}} />
-                              <span className="visually-hidden">Rating</span>
-                            </div>
-                          </div>
-                          <h2 onClick={onPlacesNameClick} className="place-card__name">
-                            <a href="#">{place.name}</a>
-                          </h2>
-                          <p className="place-card__type">Apartment</p>
-                        </div>
-                      </article>
-                    );
-                  })}
-                </div>
+                {<OffersList
+                  offers={offers}
+                />}
               </section>
               <div className="cities__right-section">
                 <section className="cities__map map" />
@@ -141,12 +106,7 @@ const App = (props) => {
 };
 
 App.propTypes = {
-  places: PropTypes.arrayOf(
-      PropTypes.shape({
-        name: PropTypes.string.isRequired,
-      })
-  ).isRequired,
-  onPlacesNameClick: PropTypes.func.isRequired,
+  offers: PropTypes.array.isRequired,
 };
 
 
