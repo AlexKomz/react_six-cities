0473ab42177710dfe951177494003ba9d5f2f856
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-03-12 16:44:29 +0300

    Создал прототип компонента Map, который просто работает

---
+++ src/components/map/map.jsx
@@ -0,0 +1,56 @@
+import React, {PureComponent, createRef} from "react";
+// import PropTypes from "prop-types";
+import leaflet from "leaflet";
+
+
+export default class Map extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this._mapRef = createRef();
+  }
+
+  render() {
+    return (
+      <div id="map" ref={this._mapRef} style={{height: `100%`}} />
+    );
+  }
+
+  componentDidMount() {
+    const city = [52.38333, 4.9];
+
+    const icon = leaflet.icon({
+      iconUrl: `img/pin.svg`,
+      iconSize: [30, 30]
+    });
+
+    const zoom = 12;
+
+    this._map = leaflet.map(this._mapRef.current, {
+      center: city,
+      zoom,
+      zoomControl: false,
+      marker: true
+    });
+
+    this._map.setView(city, zoom);
+
+    leaflet
+      .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+        attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+      })
+      .addTo(this._map);
+
+    const offerCords = [52.3709553943508, 4.89309666406198];
+
+    leaflet
+      .marker(offerCords, {icon})
+      .addTo(this._map);
+  }
+
+  componentWillUnmount() {
+    this._map.remove();
+  }
+}
+
+Map.propTypes = {};
