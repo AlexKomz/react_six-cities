73338843a418666565a6c48072c8c8064ec18a76
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-03-18 11:08:10 +0300

    Написал функцию фильтрации предложений по городу

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,9 @@ import {Raiting} from "./consts.js";
 export const convertRaitingIntoPercent = (raiting) => {
   return (raiting * 100) / Raiting.MAX;
 };
+
+export const extend = (a, b) => Object.assign({}, a, b);
+
+export const filteringOffersByCity = (offers, city) => {
+  return offers.filter((offer) => offer.city.name.toLowerCase() === city.toLowerCase());
+};
