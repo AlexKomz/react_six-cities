54d85674fa4b228157c255f40125be0613b06fb6
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-04-28 11:51:22 +0300

    Удалил лишнее

--- src/utils.js
+++ src/utils.js
@@ -1,30 +1,11 @@
-import {Raiting, SortType} from "./consts.js";
+import {Raiting} from "./consts.js";
 
 
-export const convertRaitingIntoPercent = (raiting) => {
+const convertRaitingIntoPercent = (raiting) => {
   return (raiting * 100) / Raiting.MAX;
 };
 
-export const extend = (a, b) => Object.assign({}, a, b);
+const extend = (a, b) => Object.assign({}, a, b);
 
-export const filteringOffersByCity = (offers, city) => {
-  return offers.filter((offer) => offer.city.name.toLowerCase() === city.toLowerCase());
-};
 
-export const sortingOffers = (offers, sortType) => {
-  const sortedOffers = offers.slice();
-
-  switch (sortType) {
-    case SortType.LOW_TO_HIGH:
-      sortedOffers.sort((a, b) => a.price.value - b.price.value);
-      break;
-    case SortType.HIGH_TO_LOW:
-      sortedOffers.sort((a, b) => b.price.value - a.price.value);
-      break;
-    case SortType.TOP_RATED_FIRST:
-      sortedOffers.sort((a, b) => b.rating - a.rating);
-      break;
-  }
-
-  return sortedOffers;
-};
+export {convertRaitingIntoPercent, extend};
