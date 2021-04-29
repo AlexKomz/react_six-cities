a7cd34fc82b70fd128fceefa08c2b141a71935ac
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-04-29 10:57:47 +0300

    Ведилил оставшуюся разметку в компоненты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,12 @@
 import React, {PureComponent} from "react";
+import {Switch, Route, Router} from "react-router-dom";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import classNames from "classnames";
 
+import Page from "../page/page.jsx";
+import Main from "../main/main.jsx";
+import Header from "../header/header.jsx";
 import EmptyScreen from "../empty-screen/empty-screen.jsx";
 import LocationTabsList from "../locations-tabs-list/locations-tabs-list.jsx";
 import Cities from "../cities/cities.jsx";
@@ -11,6 +15,9 @@ import withHoverOffer from "../../hocs/with-hover-offer/with-hover-offer.js";
 
 import {ActionCreator} from "../../reducer/main/main.js";
 import {getCity, getFilteredOffers} from "../../reducer/main/selectors.js";
+import {getAuthorizationStatus} from "../../reducer/user/selectors.js";
+import {AppRoute} from "../../const.js";
+import history from "../../history.js";
 
 
 const CitiesWrapped = withHoverOffer(Cities);
@@ -18,36 +25,21 @@ const CitiesWrapped = withHoverOffer(Cities);
 class App extends PureComponent {
   render() {
     return (
-      <div className="page page--gray page--main">
-        <header className="header">
-          <div className="container">
-            <div className="header__wrapper">
-              <div className="header__left">
-                <a className="header__logo-link header__logo-link--active">
-                  <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width={81} height={41} />
-                </a>
-              </div>
-              <nav className="header__nav">
-                <ul className="header__nav-list">
-                  <li className="header__nav-item user">
-                    <a className="header__nav-link header__nav-link--profile" href="#">
-                      <div className="header__avatar-wrapper user__avatar-wrapper">
-                      </div>
-                      <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </div>
-        </header>
-        {this._renderOffersScreen()}
-      </div>
+      <Router
+        history={history}
+      >
+        <Switch>
+          <Route exact path={AppRoute.ROOT}>
+            {this._renderIndexScreen()}
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 
-  _renderOffersScreen() {
+  _renderIndexScreen() {
     const {
+      authorizationStatus,
       city,
       onTabClick,
       offers,
@@ -70,6 +62,12 @@ class App extends PureComponent {
       );
     }
 
+    const pageClasses = classNames({
+      "page": true,
+      "page--gray": true,
+      "page--main": true,
+    });
+
     const mainClasses = classNames({
       "page__main": true,
       "page__main--index": true,
@@ -77,25 +75,36 @@ class App extends PureComponent {
     });
 
     return (
-      <main className={mainClasses}>
-        <h1 className="visually-hidden">Cities</h1>
-        {<LocationTabsList
-          activeCity={city}
-          onTabClick={onTabClick}
-        />}
-        {component}
-      </main>
+      <Page
+        pageClasses={pageClasses}
+      >
+        <Header
+          authorizationStatus={authorizationStatus}
+          user={null}
+        />
+        <Main
+          mainClasses={mainClasses}
+        >
+          <LocationTabsList
+            activeCity={city}
+            onTabClick={onTabClick}
+          />
+          {component}
+        </Main>
+      </Page>
     );
   }
 }
 
 App.propTypes = {
+  authorizationStatus: PropTypes.string.isRequired,
   city: PropTypes.string.isRequired,
   offers: PropTypes.array.isRequired,
   onTabClick: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
+  authorizationStatus: getAuthorizationStatus(state),
   city: getCity(state),
   offers: getFilteredOffers(state),
 });
