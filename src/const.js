a93d6f6ac9dabb118d1946b49bf087b61adea276
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-04-29 10:56:23 +0300

    Переименовал файл и добавил перечисление для путей

--- src/const.js
+++ src/const.js
@@ -1,25 +1,39 @@
-export const Raiting = {
+const Raiting = {
   MAX: 5,
 };
 
-export const Icon = {
+const Icon = {
   ICON_URL: `img/pin.svg`,
   ACTIVE_ICON_URL: `img/pin-active.svg`,
   ICON_SIZE: [30, 30],
 };
 
-export const SortType = {
+const SortType = {
   POPULAR: `Popular`,
   LOW_TO_HIGH: `Price: low to high`,
   HIGH_TO_LOW: `Price: high to low`,
   TOP_RATED_FIRST: `Top rated first`,
 };
 
-export const City = {
+const City = {
   PARIS: `Paris`,
   COLOGNE: `Cologne`,
   BRUSSELS: `Brussels`,
   AMSTERDAM: `Amsterdam`,
   HAMBURF: `Hamburg`,
   DUSSELDORF: `Dusseldorf`,
+};
+
+const AppRoute = {
+  ROOT: `/`,
+  LOGIN: `login`,
+};
+
+
+export {
+  Raiting,
+  Icon,
+  SortType,
+  City,
+  AppRoute,
 };
